5bcadd6c7ad8816dfc56b1e34afbbdad360be455
118648680+Joy060@users.noreply.github.com <fooeee778@gmail>
2026-01-16 13:52:45 +0800

    basename={process.env.PUBLIC_URL}

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,4 +1,4 @@
-import { Routes,Route} from "react-router-dom";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
 
 // 路由管制
 import PrivateRoute from "../router/PrivateRouter";
@@ -30,60 +30,62 @@ const Router = () => {
 
     return( 
         <div>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Routes>
+                    {/* 訪客首頁 */}
+                    <Route path="/" element={<Homepage />} />
 
-            <Routes>
-                {/* 訪客首頁 */}
-                <Route path="/" element={<Homepage />} />
+                    {/* 登入與註冊 */}
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
 
-                {/* 登入與註冊 */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
+                    {/* 占卜過程 */}
+                    <Route path="/pickup" element={<Pickup />} />
+                    <Route path="/Pick1A" element={<Pick1A />} />
+                    <Route path="/Pick1B" element={<Pick1B />} />
+                    <Route path="/Pick2Type" element={<Pick2Type />} />
+                    <Route path="/Pick2Input" element={<Pick2Input />} />
+                    <Route path="/PickStart" element={<PickStart />} />
+                    <Route path="/pickup/result" element={<PickupResult />} />
 
-                {/* 占卜過程 */}
-                <Route path="/pickup" element={<Pickup />} />
-                <Route path="/Pick1A" element={<Pick1A />} />
-                <Route path="/Pick1B" element={<Pick1B />} />
-                <Route path="/Pick2Type" element={<Pick2Type />} />
-                <Route path="/Pick2Input" element={<Pick2Input />} />
-                <Route path="/PickStart" element={<PickStart />} />
-                <Route path="/pickup/result" element={<PickupResult />} />
+                    {/* 關於本站 */}
+                    <Route path="/about" element={<About />} />
 
-                {/* 關於本站 */}
-                <Route path="/about" element={<About />} />
+                    {/* 學習塔羅 */}
+                    <Route path="/learn" element={<Learn />} />
 
-                {/* 學習塔羅 */}
-                <Route path="/learn" element={<Learn />} />
+                    {/* 會員首頁(需登入) */}
+                    <Route 
+                        path="/dashboard" 
+                        element={    
+                            <PrivateRoute>
+                                <Dashboard />
+                            </PrivateRoute>
+                        } 
+                    />
 
-                {/* 會員首頁(需登入) */}
-                <Route 
-                    path="/dashboard" 
-                    element={    
-                        <PrivateRoute>
-                            <Dashboard />
-                        </PrivateRoute>
-                    } 
-                />
+                    {/* 圖鑑(需登入) */}
+                    <Route 
+                        path="/collection" 
+                        element={
+                                <Collection />
+                            // <PrivateRoute>
+                            // </PrivateRoute>
+                        } 
+                    />
 
-                {/* 圖鑑(需登入) */}
-                <Route 
-                    path="/collection" 
-                    element={
-                            <Collection />
-                        // <PrivateRoute>
-                        // </PrivateRoute>
-                    } 
-                />
-
-                {/* 編輯圖片(需登入) */}
-                <Route 
-                    path="/editor" 
-                    element={
-                        <PrivateRoute>
-                            <Editor />
-                        </PrivateRoute>
-                    } 
-                />
-            </Routes>            
+                    {/* 編輯圖片(需登入) */}
+                    <Route 
+                        path="/editor" 
+                        element={
+                            <PrivateRoute>
+                                <Editor />
+                            </PrivateRoute>
+                        } 
+                    />
+                </Routes>
+            </BrowserRouter>
+            
 
         </div>
     );
